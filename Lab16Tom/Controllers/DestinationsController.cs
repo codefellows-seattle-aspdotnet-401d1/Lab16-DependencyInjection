fcc9a6b4ae8eaeb9b1a8689370d2b1b1344204f1
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-03 10:56:57 -0700

    Lab finished and readme updated

---
+++ Lab16Tom/Controllers/DestinationsController.cs
@@ -0,0 +1,25 @@
+﻿using Lab16Tom.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16Tom.Controllers
+{
+    public class DestinationsController : Controller
+    {
+        private IDestinationsRepository _repository;
+
+        public DestinationsController(IDestinationsRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_repository.Destinations);
+        }
+        
+    }
+}
