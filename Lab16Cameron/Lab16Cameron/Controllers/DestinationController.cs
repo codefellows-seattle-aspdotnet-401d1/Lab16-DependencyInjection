c09468948e25ca3dca313b4fc8d73181e8937710
Cameron Pam <cammpam.creative@gmail.com>
2017-10-03 23:57:36 -0700

    Lab 16 Cameron

---
+++ Lab16Cameron/Lab16Cameron/Controllers/DestinationController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16Cameron.Controllers
+{
+    public class DestinationController
+    {
+    }
+}
---
+++ Lab16Cameron/Lab16Cameron/Models/IDestinationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16Cameron.Models
+{
+    public interface IDestinationRepository
+    {
+        IEnumerable<Destinations> GetDestination();
+    }
+}
